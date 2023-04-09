2b5d065f1ca0efbddc5959a5943f2b87c3f9c841
Saimanbaen02 <128948704+Saimanbaen02@users.noreply.github.com>
2023-04-10 00:06:43 +0700

    Add files via upload

---
+++ API SIBKM/API SIBKM/Contexts/MyContext.cs
@@ -0,0 +1,6 @@
+﻿namespace API_SIBKM.Contexts
+{
+    public class MyContext
+    {
+    }
+}
---
+++ API SIBKM/API SIBKM/Migrations/20230409134037_IntialDatabase.Designer.cs
@@ -0,0 +1,6 @@
+﻿namespace API_SIBKM.Migrations
+{
+    public class _20230409134037_IntialDatabase
+    {
+    }
+}
